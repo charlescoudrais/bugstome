80ff876469d60a9522a400d9c8baa85b038d87f4
charles coudrais <charlescoudrais@gmail.com>
2012-10-27 23:43:05 +0200

    chco : add jquery balloon

--- public/js/myJquery.js
+++ public/js/myJquery.js
@@ -1,13 +1,35 @@
 /* 
  * My application jquery functions, listeners and events...
  */
-
 $(document).ready(function(){
+    
+    $('#user-links').balloon({
+        position: 'bottom',
+        offsetX: -30,
+        offsetY: 6,
+        css: {
+            minWidth: "100px",
+            color: '#00ff33',
+            background: '#696969',
+            border: '1px solid #ECECEC',
+            boxShadow: "0px 0px 3px #ECECEC",
+            opacity: 1
+        },
+        contents: '<ul class="ul-balloon">' +
+                  '<li><a href="">Paramètres</a></li>' +
+                  '<li><a href="">etc...</a></li>' +
+                  '</ul>' +
+                  '<hr style="margin: 10px 0px 4px 0px;" />' +
+                  '<ul class="ul-balloon">' +
+                  '<li><a href="">Déconnexion</a></li>' +
+                  '</ul>'
+    });
+    
     $('#nav li').each(function(iterator){
         $(this).click(function(){
             window.location = $(this).children('a').attr('href'); 
         });
     });
-    
 });
 
+    
