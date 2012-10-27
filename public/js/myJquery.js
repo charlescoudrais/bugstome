6d4f11a8cf9aaa53480ffabb05e27438c4bdcb68
charles coudrais <charlescoudrais@gmail.com>
2012-10-28 00:09:04 +0200

    chco : some style...

--- public/js/myJquery.js
+++ public/js/myJquery.js
@@ -9,20 +9,20 @@ $(document).ready(function(){
         offsetY: 6,
         css: {
             minWidth: "100px",
-            padding: '10px 4px',
+            padding: '10px 6px 10px 4px',
             color: '#00ff33',
             background: '#696969',
-            border: '1px solid #ECECEC',
-            boxShadow: "0px 0px 3px #ECECEC",
-            opacity: 1
+            border: '1px solid #505050',
+            boxShadow: "0px 0px 2px 1px #606060",
+            opacity: 0.9
         },
         contents: '<ul class="ul-balloon">' +
-                  '<li><a href="">Paramètres</a></li>' +
+                  '<li><a href="">YOUR_PARAMS</a></li>' +
                   '<li><a href="">etc...</a></li>' +
                   '</ul>' +
                   '<hr style="margin: 10px 0px 4px 0px;" />' +
                   '<ul class="ul-balloon">' +
-                  '<li><a href="">Déconnexion</a></li>' +
+                  '<li><a href="" style="color: orange;">LOGOUT</a></li>' +
                   '</ul>'
     });
     
