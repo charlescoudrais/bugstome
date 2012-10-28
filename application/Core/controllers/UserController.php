5e0efca71ae100dbb31268a61178c9ceac62d4ec
charles coudrais <charlescoudrais@gmail.com>
2012-10-28 19:13:54 +0100

    chco

---
+++ application/Core/controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+class UserController extends Zend_Controller_Action
+{
+    public function loginAction()
+    {
+        
+    }
+    
+    public function listAction()
+    {
+        
+    }
+}
