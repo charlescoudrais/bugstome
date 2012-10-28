15e75ed69d6a2e9b9fc85636186ec19cd3a49dce
charles coudrais <charlescoudrais@gmail.com>
2012-10-28 16:06:37 +0100

    En and Fr core translation

---
+++ application/Core/i18n/en/main.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+    
+// GLOBALS
+    'HOME_TITLE'     => 'DASHBOARD', 
+    'STATS_TITLE'    => 'STATISTICS', 
+    'PROJECTS_TITLE' => 'PROJECTS', 
+    'TASKS_TITLE'    => 'TASKS', 
+// FORMS
+    
+// STATS
+    
+// NAVIGATION
+    'NAV_HOME'    => 'Dashboard',
+    'NAV_TASK'    => 'Tasks',
+    'NAV_PROJECT' => 'Projects',
+    'NAV_STAT'    => 'Statistics',
+    
+// TEAM
+    'THE_TEAM'      => 'The Team',
+    'THE_CLIENT'    => 'The Client',
+    'THE_DEVELOPER' => 'The Developer',
+    'THE_ARTIST'    => 'The Artist',
+    'THE_VISITOR'   => 'The Visitor',
+    'UNDEFINED'     => 'John Doe',
+    
+);
