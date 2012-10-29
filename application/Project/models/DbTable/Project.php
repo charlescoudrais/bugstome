8e609ea321f30ef44e1ded8497ff01a6057dd6a8
charles coudrais <charlescoudrais@gmail.com>
2012-10-29 22:09:46 +0100

    continue project stuff...

---
+++ application/Project/models/DbTable/Project.php
@@ -0,0 +1,14 @@
+<?php
+class Project_Model_DbTable_Project extends Zend_Db_Table_Abstract
+{
+
+    public function __construct()
+    {
+        $options = array(
+            'name' => 'projects',
+            'primary' => 'p_id', 
+        );
+        
+        parent::__construct($options);
+    }
+}
--- application/Project/models/Project.php
+++ application/Project/models/Project.php
@@ -1,5 +1,65 @@
 <?php
 class Project_Model_Project
 {
+    /**
+     *
+     * @var int 
+     */
+    private $projectId;
+    /**
+     *
+     * @var string 
+     */
+    private $projectName;
+    /**
+     *
+     * @var string 
+     */
+    private $projectManager;
+    /**
+     *
+     * @var string 
+     */
+    private $projectDescription;
     
+    /**
+     * 
+     * @return type
+     */
+    public function getProjectId() {
+        return $this->projectId;
+    }
+    
+
+    public function setProjectId($projectId) {
+        $this->projectId = $projectId;
+        return $this;
+    }
+
+    public function getProjectName() {
+        return $this->projectName;
+    }
+
+    public function setProjectName($projectName) {
+        $this->projectName = $projectName;
+        return $this;
+    }
+
+    public function getProjectManager() {
+        return $this->projectManager;
+    }
+
+    public function setProjectManager($projectManager) {
+        $this->projectManager = $projectManager;
+        return $this;
+    }
+
+    public function getProjectDescription() {
+        return $this->projectDescription;
+    }
+
+    public function setProjectDescription($projectDescription) {
+        $this->projectDescription = $projectDescription;
+        return $this;
+    }
 }
