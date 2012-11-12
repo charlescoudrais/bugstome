9cfb2167d0abdcc510427e3bb50cc84be04e68c6
charles coudrais <charlescoudrais@gmail.com>
2012-11-12 21:49:50 +0100

    Commit tables insert statement (MySQL)

---
+++ datas.sql
@@ -0,0 +1,94 @@
+DELIMITER $$
+
+USE `bugsme`
+$$
+
+-- Priorities
+INSERT INTO `bugsme`.`priorities`(
+	`priority_id`,
+	`priority_name`
+)
+VALUES(
+	1,
+	'Low'
+)
+$$
+
+INSERT INTO `bugsme`.`priorities`(
+	`priority_id`,
+	`priority_name`
+)
+VALUES(
+	2,
+	'Normal'
+)
+$$
+
+INSERT INTO `bugsme`.`priorities`(
+	`priority_id`,
+	`priority_name`
+)
+VALUES(
+	3,
+	'Urgent'
+)
+$$
+
+-- Roles
+INSERT INTO `bugsme`.`roles`(
+	`role_id`,
+	`role_name`
+)
+VALUES(
+	1,
+	'Root'
+)
+$$
+
+INSERT INTO `bugsme`.`roles`(
+	`role_id`,
+	`role_name`
+)
+VALUES(
+	2,
+	'Admin'
+)
+$$
+
+INSERT INTO `bugsme`.`roles`(
+	`role_id`,
+	`role_name`
+)
+VALUES(
+	3,
+	'User'
+)
+$$
+
+INSERT INTO `bugsme`.`roles`(
+	`role_id`,
+	`role_name`
+)
+VALUES(
+	4,
+	'Guest'
+)
+$$
+
+-- Users
+INSERT INTO `bugsme`.`users`(
+	`user_id`,
+	`user_name`,
+	`user_password`,
+	`user_active`,
+	`role_id`)
+VALUES
+(
+	1,
+	'Admin',
+	'5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8',
+	1,
+	1
+)
+$$
+
