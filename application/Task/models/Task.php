a13e41d4599bf1a381a85d152d80d61cb54dac6a
charles coudrais <charlescoudrais@gmail.com>
2012-11-28 22:14:58 +0100

    update task model
    Remember to change my netbean's configuration

--- application/Task/models/Task.php
+++ application/Task/models/Task.php
@@ -15,7 +15,22 @@ class Task_Model_Task
      *
      * @var string 
      */
+    private $taskStart;
+    /**
+     *
+     * @var string 
+     */
+    private $taskEnd;
+    /**
+     *
+     * @var int 
+     */
     private $taskManager;
+    /**
+     *
+     * @var int 
+     */
+    private $taskPriority;
     /**
      *
      * @var string 
@@ -32,51 +47,92 @@ class Task_Model_Task
      * 
      * @return type
      */
-    public function getTaskId() {
+    public function getTaskId()
+    {
         return $this->taskId;
     }
     
 
-    public function setTaskId($taskId) {
+    public function setTaskId($taskId)
+    {
         $this->taskId = $taskId;
         return $this;
     }
 
-    public function getTaskName() {
+    public function getTaskName()
+    {
         return $this->taskName;
     }
 
-    public function setTaskName($taskName) {
+    public function setTaskName($taskName)
+    {
         $this->taskName = $taskName;
         return $this;
     }
+    
+    public function getTaskStart()
+    {
+        return $this->taskStart;
+    }
 
-    public function getTaskManager() {
+    public function setTaskStart($taskStart)
+    {
+        $this->taskStart = $taskStart;
+        return $this;
+    }
+
+    public function getTaskEnd()
+    {
+        return $this->taskEnd;
+    }
+
+    public function setTaskEnd($taskEnd)
+    {
+        $this->taskEnd = $taskEnd;
+        return $this;
+    }
+
+    public function getTaskManager()
+    {
         return $this->taskManager;
     }
 
-    public function setTaskManager($taskManager) {
+    public function setTaskManager($taskManager)
+    {
         $this->taskManager = $taskManager;
         return $this;
     }
 
-    public function getTaskDescription() {
+    public function getTaskDescription()
+    {
         return $this->taskDescription;
     }
 
-    public function setTaskDescription($taskDescription) {
+    public function setTaskDescription($taskDescription)
+    {
         $this->taskDescription = $taskDescription;
         return $this;
     }
     
-    public function getTaskMapper() {
+    public function getTaskPriority() {
+        return $this->taskPriority;
+    }
+
+    public function setTaskPriority($taskPriority) {
+        $this->taskPriority = $taskPriority;
+        return $this;
+    }
+
+    public function getTaskMapper()
+    {
         if (NULL === $this->taskMapper) {
             $this->taskMapper = new Task_Model_Mapper_Task();
         }
         return $this->taskMapper;
     }
 
-    public function setTaskMapper($taskMapper) {
+    public function setTaskMapper($taskMapper)
+    {
         $this->taskMapper = $taskMapper;
         return $this;
     }
--- application/Task/models/mappers/Task.php
+++ application/Task/models/mappers/Task.php
@@ -1,8 +1,108 @@
 <?php
 class Task_Model_Mapper_Task
 {
+    private $dbTable;
+    
+    public function lastInsertId()
+    {
+        $sql = "SELECT MAX(task_id) AS id FROM tasks";
+        $rowSet = $this->getDbTable()->getAdapter()->query($sql);
+        if (0 === count($rowSet)) {
+            return false;
+        }
+        foreach ($rowSet as $row) {
+             $res = $row['id'] + 1;
+             break;
+        }
+        return $res;
+    }
+    
     public function find($id)
     {
-        return $id;
+        $rowSet = $this->getDbTable()->find($id);
+        $row = $rowSet->current();
+        if (0 === count($row)) {
+            return false;
+        }
+        return $this->rowToObject($row);
+    }
+    
+    public function fetchAll()
+    {
+        $rowSet = $this->getDbTable()->fetchAll();
+        if (0 === count($rowSet)) {
+            return false;
+        }
+        $tasks = array();
+        foreach ($rowSet as $row) {
+             $tasks[] = $this->rowToObject($row);
+        }
+        return $tasks;
+    }    
+    
+    public function save($task)
+    {
+        $row = $this->objectToRow($task);
+        
+        if (0 === (int) $task->getId()) {
+            unset($row['task_id']);
+            $this->getDbTable()->insert($row);
+            return $this->getDbTable()->getAdapter()->lastInsertId();
+        } else {
+            $this->getDbTable()
+                 ->update(
+                      $row, 
+                      array( 'task_id = ?' => $task->getId())
+                  );
+            return $task->getId();
+        }
+    }
+    
+    public function delete($task)
+    {
+        return $this->getDbTable()->delete(
+                    array('task_id = ?' => $task->getId())
+                );
+    }
+    
+    public function objectToRow($task)
+    {
+        return array( 
+            'task_id'          => $task->getTaskId(),
+            'task_title'       => $task->getTaskName(),
+            'task_start'       => $task->getTaskStart(),
+            'task_end'         => $task->getTaskEnd(),
+            'task_description' => $task->getTaskDescription(),
+            'user_id'          => $task->getTaskManager(),
+            'priority_id'      => $task->getTaskPriority(),
+        );
+    }
+    
+    public function rowToObject($row)
+    {
+         
+         $task = new Task_Model_Task();
+         $task->setTaskId($row->task_id)
+              ->setTaskName($row->task_title)
+              ->setTaskStart($row->task_start)
+              ->setTaskEnd($row->task_end)
+              ->setTaskDescription($row->task_description)
+              ->setTaskManager($row->user_id)
+              ->setTaskPriority($row->priority_id);
+         return $task;
+    }
+    
+    public function getDbTable()
+    {
+        if (null === $this->dbTable) {
+            $this->dbTable = new Task_Model_DbTable_Task();
+        }
+        return $this->dbTable;
+    }
+    
+    public function setDbTable($dbTable)
+    {
+        $this->dbTable = $dbTable;
+        return $this;
     }
 }
