46c12d8155a93c47cdf7261c846c15a0790fe97e
charles coudrais <charlescoudrais@gmail.com>
2012-12-02 10:08:31 +0100

    chco

--- application/Task/controllers/NoteController.php
+++ application/Task/controllers/NoteController.php
@@ -14,13 +14,15 @@ class Task_NoteController extends Zend_Controller_Action
     
     public function noteAction()
     {
+        $this->_initLayout();
+        
         $noteId = (int) $this->getRequest()->getParam('id');
         $taskId = (int) $this->getRequest()->getParam('tid');
         $this->view->noteId = $noteId;
         $this->view->taskId = $taskId;
         $this->view->pageTitle = $this->view->translate('NOTE_NOTE_TITLE');
         
-        if ($noteId === 0 && $taskId === 0) {
+        if ($taskId === 0) {
             $this->view->tasks = $this->tasks;
         }
         
@@ -28,8 +30,27 @@ class Task_NoteController extends Zend_Controller_Action
     
     public function listAction()
     {
+        $this->_initLayout();
+        
         $this->view->pageTitle = $this->view->translate('NOTE_LIST_TITLE');
         
         $this->view->notes = $this->notes;
+    }
+    
+    private function _initLayout($path = null)
+    {
+        if ($path === NULL) {
+            $path = ROOT_PATH . DIRECTORY_SEPARATOR
+                    . 'application' . DIRECTORY_SEPARATOR
+                    . 'Task' . DIRECTORY_SEPARATOR
+                    . 'views' . DIRECTORY_SEPARATOR
+                    . 'layouts';
+        }
+        
+        $this->_helper->layout()
+                      ->setLayoutPath($path)
+                      ->setLayout('layout');
+        
+        return TRUE;
     }
 }
