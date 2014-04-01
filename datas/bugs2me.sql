d5ce436fa66ab3bb97302ca482d42532e45e1796
Charles Coudrais <charlescoudrais@MacBook-Pro-de-Charles.local>
2014-04-01 12:09:28 +0200

    01042014-1209

--- datas/bugs2me.sql
+++ datas/bugs2me.sql
@@ -1,51 +1,89 @@
-CREATE DATABASE  IF NOT EXISTS `bugstome` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `bugstome`;
--- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
---
--- Host: localhost    Database: bugstome
--- ------------------------------------------------------
--- Server version	5.6.14
+/*
+DROP DATABASE IF EXISTS bugs2me;
+CREATE DATABASE IF NOT EXISTS `bugs2me` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*
+USE `bugs2me`;
+*/
 
 --
--- Table structure for table `notes`
+-- Table structure for table `roles`
 --
 
-DROP TABLE IF EXISTS `notes`;
+DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `notes` (
-  `note_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `note_label` varchar(45) NOT NULL,
-  `note_description` text,
-  `note_date` varchar(45) NOT NULL,
-  `task_id` int(10) unsigned NOT NULL,
-  `user_id` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`note_id`),
-  KEY `fk_task_id_idx` (`task_id`),
-  KEY `fk_user_id_idx` (`user_id`),
-  CONSTRAINT `fk_nuser_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_task_id` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON UPDATE CASCADE
+CREATE TABLE `roles` (
+  `role_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `notes`
+-- Dumping data for table `roles`
 --
 
-LOCK TABLES `notes` WRITE;
-/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Root'),(2,'Admin'),(3,'User'),(4,'Guest');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) NOT NULL,
+  `user_mail` varchar(70) NOT NULL,
+  `user_password` char(42) NOT NULL,
+  `user_function` varchar(45) NOT NULL DEFAULT 'N.R.',
+  `user_active` tinyint(4) NOT NULL DEFAULT '0',
+  `role_id` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_mail` (`user_mail`),
+  KEY `fk_role_id` (`role_id`),
+  CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Admin','admin@test.fr','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8','Administrator',1,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `status` (
+  `status_id` tinyint(4) NOT NULL,
+  `status_name` varchar(45) NOT NULL,
+  `status_desc` varchar(150) NOT NULL DEFAULT 'description',
+  PRIMARY KEY (`status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -59,7 +97,7 @@ CREATE TABLE `priorities` (
   `priority_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `priority_name` varchar(45) NOT NULL,
   PRIMARY KEY (`priority_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +130,7 @@ CREATE TABLE `projects` (
   KEY `fk_users_projects_id_idx` (`user_id`),
   CONSTRAINT `fk_priorities_projects_id` FOREIGN KEY (`priority_id`) REFERENCES `priorities` (`priority_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_projects_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,32 +139,8 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,'My first project','17-10-2012 22:50:15','21-12-2012 00:00:01','This is an exemple project...',1,2);
+INSERT INTO `projects` VALUES (1,'Bugstome','17-10-2012 22:50:15','24-07-2014 00:00:01','Groupware, project management application...',1,2);
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roles` (
-  `role_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(25) NOT NULL,
-  PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'Root'),(2,'Admin'),(3,'User'),(4,'Guest');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -152,7 +166,7 @@ CREATE TABLE `tasks` (
   CONSTRAINT `fk_priority_id` FOREIGN KEY (`priority_id`) REFERENCES `priorities` (`priority_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,49 +175,37 @@ CREATE TABLE `tasks` (
 
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'Task 1 (Project 1)','12-12-2012 12:12:12','21-12-2012 12:12:12','description...',1,2,1),(2,'Task 2 (Project 1)','12-12-2012 12:12:12','21-12-2012 12:12:12','description...',1,1,1);
+INSERT INTO `tasks` VALUES (1,'Test project saving','12-12-2012 12:12:12','01-05-2014 12:12:12','description...',1,2,1),(2,'Test task saving','12-12-2012 12:12:12','21-12-2012 12:12:12','description...',1,1,1);
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `users`
+-- Table structure for table `notes`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `notes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `user_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(45) NOT NULL,
-  `user_mail` varchar(70) NOT NULL,
-  `user_password` char(42) NOT NULL,
-  `user_function` varchar(45) NOT NULL DEFAULT 'N.R.',
-  `user_active` tinyint(4) NOT NULL DEFAULT '0',
-  `role_id` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_mail` (`user_mail`),
-  KEY `fk_role_id` (`role_id`),
-  CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE TABLE `notes` (
+  `note_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `note_label` varchar(45) NOT NULL,
+  `note_description` text,
+  `note_date` varchar(45) NOT NULL,
+  `task_id` int(10) unsigned NOT NULL,
+  `user_id` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`note_id`),
+  KEY `fk_task_id_idx` (`task_id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_nuser_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_task_id` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `notes`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Admin','admin@test.fr','5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8','Administrator',1,1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-03-30 19:09:06
--- datas/schemas.sql
+++ datas/schemas.sql
@@ -1,4 +1,9 @@
-CREATE TABLE `notes` (
+- 
+-- Structure de la table `notes`
+-- 
+
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE IF NOT EXISTS `notes` (
   `note_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `note_label` varchar(45) NOT NULL,
   `note_description` text,
@@ -21,11 +26,12 @@ CREATE TABLE `notes` (
 -- Structure de la table `priorities`
 -- 
 
-CREATE TABLE `priorities` (
+DROP TABLE IF EXISTS `priorities`;
+CREATE TABLE IF NOT EXISTS `priorities` (
   `priority_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `priority_name` varchar(45) NOT NULL,
   PRIMARY KEY (`priority_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
 -- Contenu de la table `priorities`
@@ -41,21 +47,25 @@ INSERT INTO `priorities` (`priority_id`, `priority_name`) VALUES (3, 'Urgent');
 -- Structure de la table `projects`
 -- 
 
-CREATE TABLE `projects` (
+DROP TABLE IF EXISTS `projects`;
+CREATE TABLE IF NOT EXISTS `projects` (
   `project_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_title` varchar(45) NOT NULL,
   `project_start` varchar(45) NOT NULL,
   `project_end` varchar(45) NOT NULL,
   `project_description` varchar(500) NOT NULL,
   `user_id` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `priority_id` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`project_id`),
+  KEY `fk_priorities_projects_id_idx` (`priority_id`),
+  KEY `fk_users_projects_id_idx` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
 -- Contenu de la table `projects`
 -- 
 
-INSERT INTO `projects` (`project_id`, `project_title`, `project_start`, `project_end`, `project_description`, `user_id`) VALUES (1, 'My first project', '17-10-2012 22:50:15', '21-12-2012 00:00:01', 'This is an exemple project...', 1);
+INSERT INTO `projects` (`project_id`, `project_title`, `project_start`, `project_end`, `project_description`, `user_id`, `priority_id`) VALUES (1, 'Bugstome', '17-10-2012 22:50:15', '24-07-2014 00:00:01', 'Groupware, project management application...', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -63,11 +73,12 @@ INSERT INTO `projects` (`project_id`, `project_title`, `project_start`, `project
 -- Structure de la table `roles`
 -- 
 
-CREATE TABLE `roles` (
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
   `role_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `role_name` varchar(25) NOT NULL,
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
 -- Contenu de la table `roles`
@@ -81,10 +92,30 @@ INSERT INTO `roles` (`role_id`, `role_name`) VALUES (4, 'Guest');
 -- --------------------------------------------------------
 
 -- 
+-- Structure de la table `status`
+-- 
+
+DROP TABLE IF EXISTS `status`;
+CREATE TABLE IF NOT EXISTS `status` (
+  `status_id` tinyint(4) NOT NULL,
+  `status_name` varchar(45) NOT NULL,
+  `status_desc` varchar(150) NOT NULL DEFAULT 'description',
+  PRIMARY KEY (`status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 
+-- Contenu de la table `status`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Structure de la table `tasks`
 -- 
 
-CREATE TABLE `tasks` (
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
   `task_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `task_label` varchar(100) NOT NULL,
   `task_start` varchar(45) NOT NULL,
@@ -97,14 +128,14 @@ CREATE TABLE `tasks` (
   KEY `fk_user_id_idx` (`user_id`),
   KEY `fk_priority_id_idx` (`priority_id`),
   KEY `fk_project_id_idx` (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
 -- Contenu de la table `tasks`
 -- 
 
-INSERT INTO `tasks` (`task_id`, `task_label`, `task_start`, `task_end`, `task_description`, `user_id`, `priority_id`, `project_id`) VALUES (1, 'Task 1 (Project 1)', '12-12-2012 12:12:12', '21-12-2012 12:12:12', 'description...', 1, 2, 1);
-INSERT INTO `tasks` (`task_id`, `task_label`, `task_start`, `task_end`, `task_description`, `user_id`, `priority_id`, `project_id`) VALUES (2, 'Task 2 (Project 1)', '12-12-2012 12:12:12', '21-12-2012 12:12:12', 'description...', 1, 1, 1);
+INSERT INTO `tasks` (`task_id`, `task_label`, `task_start`, `task_end`, `task_description`, `user_id`, `priority_id`, `project_id`) VALUES (1, 'Test project saving', '12-12-2012 12:12:12', '01-05-2014 12:12:12', 'description...', 1, 2, 1);
+INSERT INTO `tasks` (`task_id`, `task_label`, `task_start`, `task_end`, `task_description`, `user_id`, `priority_id`, `project_id`) VALUES (2, 'Test task saving', '12-12-2012 12:12:12', '21-12-2012 12:12:12', 'description...', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -112,7 +143,8 @@ INSERT INTO `tasks` (`task_id`, `task_label`, `task_start`, `task_end`, `task_de
 -- Structure de la table `users`
 -- 
 
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `user_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(45) NOT NULL,
   `user_mail` varchar(70) NOT NULL,
@@ -123,7 +155,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_mail` (`user_mail`),
   KEY `fk_role_id` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
 -- Contenu de la table `users`
@@ -141,6 +173,13 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_mail`, `user_password`, `user
 ALTER TABLE `notes`
   ADD CONSTRAINT `fk_nuser_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_task_id` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON UPDATE CASCADE;
+
+-- 
+-- Contraintes pour la table `projects`
+-- 
+ALTER TABLE `projects`
+  ADD CONSTRAINT `fk_priorities_projects_id` FOREIGN KEY (`priority_id`) REFERENCES `priorities` (`priority_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_projects_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- 
 -- Contraintes pour la table `tasks`
